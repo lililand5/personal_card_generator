d5b56677030fbce1efcd30d4f2607b3f5bd9a83f
Liliya <tor-ver@mail.ru>
2021-11-10 10:34:57 +0300

    Добавлен метод get_phone

--- methods.rb
+++ methods.rb
@@ -4,25 +4,41 @@ def get_name
     puts "Введите фамилию имя и отчество:"
     name = STDIN.gets.chomp
 
-  end until name=~/\A(?=\A.{,100}\z) *[a-zа-яё]+ +[a-zа-яё]+ +[a-zа-яё]+ *\z/i
+  end until name =~ /\A(?=\A.{,100}\z) *[a-zа-яё]+ +[a-zа-яё]+ +[a-zа-яё]+ *\z/i
   # Regexp - не позволяет строке быть длиннее 100 символов
   # при этом позволяет вводить лишние пробелы
 
   name = name.scan(/[a-zа-яё]+/i).map(&:capitalize).join(" ")
   # Строка очищается от лишних пробелов, форматируется регистр
-
 end
 
 
 def get_description
 
   begin
-    puts "Опишите деятельность человека:"
+    puts "Опишите деятельность человека(не более 60 символов):"
     description = STDIN.gets.chomp
 
-  end while description=~/<.+>/
-  #regexp проверяет наличие вредоносного кода
+  end while description.size >= 60 && description =~ /<.+>/
+  # regexp проверяет наличие вредоносного кода
 
+  description.strip!
   description[0] = description[0].upcase
+  # Описание всегда будет начинаться с большой буквы
+  # и не будет содержать пробелы в начале и в конце строки
 
+  description
+end
+
+
+def get_phone
+
+  begin
+    puts "Введите номер телефона(только цифры):"
+    print "+7"
+    phone = STDIN.gets.chomp
+
+  end until phone =~ /\A\d{10}\z/
+
+  "Телефон: +7 #{phone[0, 3]} #{phone[3, 3]}-#{phone[6, 2]}-#{phone[8, 2]}"
 end
