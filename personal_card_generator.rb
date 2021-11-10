60cf50bc03b31deb59ad8502fe77f8c099388e3b
Liliya <tor-ver@mail.ru>
2021-11-10 09:18:44 +0300

    Вынесение методов в отдельный файл

--- personal_card_generator.rb
+++ personal_card_generator.rb
@@ -4,14 +4,4 @@ data = []
 
 data << get_name << get_description << get_phone << get_email
 
-begin
-  puts "Введите фамилию имя и отчество:"
- # name = STDIN.gets.chomp
-  name = STDIN.gets.chompd
 
-end until name=~/\A(?=\A.{,100}\z) *[a-zа-яё]+ +[a-zа-яё]+ +[a-zа-яё]+ *\z/i
-# Regexp - не позволяет строке быть длиннее 100 символов
-# при этом позволяет вводить лишние пробелы
-
-name = name.scan(/[a-zа-яё]+/).map(&:capitalize).join(" ")
-# Строка очищается от лишних пробелов, форматируется регистр
