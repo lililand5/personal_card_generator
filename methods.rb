f12a85497dfb3448dd44881c516b8f4c9ea198db
Liliya <tor-ver@mail.ru>
2021-11-10 10:14:52 +0300

    Добавлен метод get_description

--- methods.rb
+++ methods.rb
@@ -5,10 +5,24 @@ def get_name
     name = STDIN.gets.chomp
 
   end until name=~/\A(?=\A.{,100}\z) *[a-zа-яё]+ +[a-zа-яё]+ +[a-zа-яё]+ *\z/i
-# Regexp - не позволяет строке быть длиннее 100 символов
-# при этом позволяет вводить лишние пробелы
+  # Regexp - не позволяет строке быть длиннее 100 символов
+  # при этом позволяет вводить лишние пробелы
 
   name = name.scan(/[a-zа-яё]+/i).map(&:capitalize).join(" ")
-# Строка очищается от лишних пробелов, форматируется регистр
+  # Строка очищается от лишних пробелов, форматируется регистр
+
+end
+
+
+def get_description
+
+  begin
+    puts "Опишите деятельность человека:"
+    description = STDIN.gets.chomp
+
+  end while description=~/<.+>/
+  #regexp проверяет наличие вредоносного кода
+
+  description[0] = description[0].upcase
 
 end
